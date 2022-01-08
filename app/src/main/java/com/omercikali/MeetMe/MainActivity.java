d103c7b798ee5f105d4037c3273ccabab3d636c7
omercikali <63413613+omercikali@users.noreply.github.com>
2022-01-08 13:13:19 +0300

    base

---
+++ app/src/main/java/com/omercikali/MeetMe/MainActivity.java
@@ -0,0 +1,14 @@
+package com.omercikali.MeetMe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
